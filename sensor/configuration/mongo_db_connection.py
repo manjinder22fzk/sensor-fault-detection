dc2c9a32a0ba0dd6693a450cf802547deefbf3fa
manjinder22fzk <manjindersinghfzk@gmail.com>
2024-01-01 16:38:28 +0530

    Directory Creation

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,7 +1,6 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
 import certifi
-from urllib.parse import quote_plus
 
 ca = certifi.where()
 
@@ -13,11 +12,7 @@ class MongoDBClient:
         try:
 
             if MongoDBClient.client is None:
-                username = "manjinder_singh"
-                password = "Engineering96@"
-                escaped_username = quote_plus(username)
-                escaped_password = quote_plus(password)
-                MONGO_DB_URL = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.ftk5qa5.mongodb.net/?retryWrites=true&w=majority"
+
                 mongo_db_url = MONGO_DB_URL
                 print(mongo_db_url)
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
---
+++ sensor/constant/env_variable.py
@@ -0,0 +1,15 @@
+from urllib.parse import quote_plus
+
+
+username = "manjinder_singh"
+password = "Engineering96@"
+escaped_username = quote_plus(username)
+escaped_password = quote_plus(password)
+MONGO_DB_URL = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.ftk5qa5.mongodb.net/?retryWrites=true&w=majority"
+
+
+MONGODB_URL_KEY = "MONGO_DB_URL"
+# AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+# AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+# REGION_NAME = "us-east-1"
+
